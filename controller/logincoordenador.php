614f9977413c6ab1f9a232acce32e2fadbdc8c7f
Arthur José Fernandes <49981282+arthurjf@users.noreply.github.com>
2022-07-20 21:41:05 -0300

    Correcao no login

--- controller/logincoordenador.php
+++ controller/logincoordenador.php
@@ -1,18 +1,24 @@
 <?php
 include_once("../classes/manipuladados.php");
+session_start();
 
 $manipula = new manipuladados();
+$manipula->setTable("tb_coordenador");
 
 $login = $_POST['txtEmail'];
 $senha = $_POST['txtSenha'];
 
-$linhas = $manipula->validarLogin($login, $senha);
+$dadosLogin = $manipula->validarLogin($login, $senha);
 
-if ($linhas == 0) {
+if ($dadosLogin == null) {
     echo '<script>alert("Nome ou senha não cadastrada ou incorreta")</script>';
     echo "<script>location = '../index.php';</script>";
 } else {
-    setcookie("email_usuario", $login);
-    setcookie("senha_usuario", $senha);
-    header("location: ../coordenador/index.php");
+    $_SESSION["id"] = $dadosLogin->id;
+    $_SESSION["idPessoa"] = $dadosLogin->idPessoa;
+    $_SESSION["nome"] = $dadosLogin->nome;
+    $_SESSION["email"] = $dadosLogin->email;
+    setcookie("email_usuario", $dadosLogin->email);
+    setcookie("senha_usuario", $password);
+    header("location: ../aluno/index.php");
 }
